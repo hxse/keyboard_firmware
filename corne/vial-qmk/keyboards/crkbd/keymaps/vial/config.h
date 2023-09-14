89596764fd2f4b7788005f479b383f97e96ce87a
hxse <narutohy@163.com>
2023-09-15 00:15:14 +0800

    update

--- corne/vial-qmk/keyboards/crkbd/keymaps/vial/config.h
+++ corne/vial-qmk/keyboards/crkbd/keymaps/vial/config.h
@@ -60,10 +60,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     {                \
         27, 27       \
     }
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+#define RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_HUE_STEP 4
+#define RGBLIGHT_SAT_STEP 10
+#define RGBLIGHT_VAL_STEP 10
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
