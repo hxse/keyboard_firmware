4b8a8cf7374eb94403e6ea6f9e913a16042d3d19
hxse <narutohy@163.com>
2023-09-24 18:14:02 +0800

    update

--- corne/run-vial-qmk.bat
+++ corne/run-vial-qmk.bat
@@ -1,2 +1,5 @@
+cd D:\App\keyboard
+git clone --recurse-submodules https://github.com/vial-kb/vial-qmk
+
 copy /Y "D:\my_repo\keyboard_firmware\corne\vial-qmk\keyboards\crkbd\keymaps\vial\*.*" "D:\App\keyboard\vial-qmk\keyboards\crkbd\keymaps\vial\"
 d:\App\keyboard\QMK_MSYS\qmk_msys\shell_connector.cmd -c 'cd D:/App/keyboard/vial-qmk;qmk compile -kb crkbd -km vial -e CONVERT_TO=helios;start D:/App/keyboard/vial-qmk/.build'
