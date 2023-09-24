fa6012765e2fdfe0d1892168639c2319f0c5628f
hxse <narutohy@163.com>
2023-09-24 17:32:42 +0800

    update

--- corne/vial-qmk/keyboards/crkbd/keymaps/vial/rules.mk
+++ corne/vial-qmk/keyboards/crkbd/keymaps/vial/rules.mk
@@ -6,7 +6,7 @@ RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = yes
 OLED_ENABLE         = yes
-#OLED_DRIVER         = SSD1306
+OLED_DRIVER         = SSD1306
 EXTRAKEY_ENABLE     = yes
 COMBO_ENABLE        = no
 
